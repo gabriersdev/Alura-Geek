7f8ee6a0121fde7b7eadf321ebaf95156a5ca561
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-01-18 10:10:28 -0300

    Adicionando botão de controle de senha, ajustando a margem e alterando o select

--- assets/js/login.js
+++ assets/js/login.js
@@ -24,11 +24,20 @@
     inputSenha.focus();
   }
 
-  const btnSenhas = document.querySelectorAll('[data-formulario-senha]');
-  btnSenhas.forEach(botao => {
-    botao.addEventListener('click', (evento) => {
+  const btnControleSenha = document.querySelector('[data-formulario-senha]');
+  btnControleSenha.addEventListener('click', (evento) => {
+    const input = btnControleSenha.parentElement.querySelector('input');
+    
+    if(input.type == 'password'){
+      input.type = 'text';
+      btnControleSenha.querySelector('i').setAttribute('class', 'bi bi-eye-slash-fill');
+    }else{
+      input.type = 'password';
+      btnControleSenha.querySelector('i').setAttribute('class', 'bi bi-eye-fill');
+    }
 
-    })
+    input.focus();
+    evento.preventDefault();
   })
 
 })();
