3007045c5e030389f0d714880a6dfb0fc83d4e3d
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-01-18 11:10:32 -0300

    Implementando validação de formulário da página de login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,29 +1,5 @@
 (() => {
   
-  function controleSenha(condicao, idInputSenha){
-    //Campo de entrada da senha
-    let inputSenha = document.querySelector(idInputSenha);  
-    //Botão que aciona a função
-    let btnControleSenha = document.querySelector('#btn-controle-senha');
-    //Icon do olho (biblioteca usada: Bootstrap Icons)
-    let iconControleSenha = document.querySelector('#icon-controle-senha');
-    
-    //Para exibir a senha
-    if(condicao == 'mostrar'){ 
-      inputSenha.setAttribute('type', 'text');
-      let atributosEsconderSenha = "controleSenha('esconder','" + idInputSenha + "')";
-      btnControleSenha.setAttribute('onclick', atributosEsconderSenha);
-      iconControleSenha.setAttribute('class', 'bi bi-eye-slash-fill');
-    }else{
-      inputSenha.setAttribute('type', 'password');
-      let atributosMostrarSenha = "controleSenha('mostrar','" + idInputSenha + "')";
-      btnControleSenha.setAttribute('onclick', atributosMostrarSenha);
-      iconControleSenha.setAttribute('class', 'bi bi-eye-fill');
-    }
-    
-    inputSenha.focus();
-  }
-
   const btnControleSenha = document.querySelector('[data-formulario-senha]');
   btnControleSenha.addEventListener('click', (evento) => {
     const input = btnControleSenha.parentElement.querySelector('input');
---
+++ assets/js/tratamento-formulario.js
@@ -0,0 +1,26 @@
+(() => {
+
+  const inputs = document.querySelectorAll('[data-input]');
+  inputs.forEach(input => {
+    
+    const campoFeedback = input.parentElement.querySelector('span') || input.parentElement.parentElement.querySelector('span');
+
+    input.addEventListener('keyup', (evento) => {
+      const valido = evento.target.validity.valid; //Se tudo está válido
+      const requerido = evento.target.validity.valueMissing; //Verifica se há required e se não está preenchido
+      const tipo = evento.target.validity.typeMismatch; //O valor correponde ao tipo
+      
+      if(tipo){
+        campoFeedback.classList.toggle('none');
+        campoFeedback.textContent = `${input.getAttribute('placeholder')} está inválido`;
+      }
+
+      if(requerido){
+        campoFeedback.classList.toggle('none');
+        campoFeedback.textContent = 'Preenchimento necessário';
+      }
+
+    })
+  })
+
+})();
